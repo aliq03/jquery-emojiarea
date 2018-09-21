72cf7e823bec70131485c952aba00e3729291c5d
aliq03 <aliq03@hotmail.com>
2018-09-21 19:05:23 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,9 @@
+﻿$('.emojis-plain').emojiarea({wysiwyg: false});
+		
+		var $wysiwyg = $('.emojis-wysiwyg').emojiarea({wysiwyg: true});
+		var $wysiwyg_value = $('.emojis-wysiwyg-value');
+		
+		$wysiwyg.on('change', function() {
+			$wysiwyg_value.text($(this).val());
+		});
+		$wysiwyg.trigger('change');
