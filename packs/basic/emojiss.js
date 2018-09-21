a670bc488a5c8cf306e78fc5cafebcd12d089b86
aliq03 <aliq03@hotmail.com>
2018-09-21 18:50:18 +0300

    Update and rename emojiss.js to emojisss.js

--- packs/basic/emojiss.js
+++
@@ -1,8 +0,0 @@
-$.emojiarea.path = '../packs/basic';
-$.emojiarea.icons = {
-	':smile:'     : 'https://www.bedava-sitem.com/images.https://img.webme.com/pic/a/aliq03/smile.png',
-	':angry:'     : 'https://www.bedava-sitem.com/images.https://img.webme.com/pic/a/aliq03/angry.png',
-	':flushed:'   : 'https://www.bedava-sitem.com/images.https://img.webme.com/pic/a/aliq03/flushed.png',
-	':neckbeard:' : 'https://www.bedava-sitem.com/images.https://img.webme.com/pic/a/aliq03/neckbeard.png',
-	':laughing:'  : 'https://www.bedava-sitem.com/images.https://img.webme.com/pic/a/aliq03/laughing.png'
-};
---
+++ packs/basic/emojisss.js
@@ -0,0 +1,8 @@
+$.emojiarea.path = 'https://cdn.rawgit.com/aliq03/jquery-emojiarea/tree/master/packs/basic';
+$.emojiarea.icons = {
+	':smile:'     : 'https://img.webme.com/pic/a/aliq03/smile.png',
+	':angry:'     : 'https://img.webme.com/pic/a/aliq03/angry.png',
+	':flushed:'   : 'https://img.webme.com/pic/a/aliq03/flushed.png',
+	':neckbeard:' : 'https://img.webme.com/pic/a/aliq03/neckbeard.png',
+	':laughing:'  : 'https://img.webme.com/pic/a/aliq03/laughing.png'
+};
