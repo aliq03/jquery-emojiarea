34670a4656e8b6c26c82142fd714878a734a03f8
aliq03 <aliq03@hotmail.com>
2018-09-22 09:33:48 +0300

    Update and rename emojix.js to emojixx.js

--- packs/basic/emojixx.js
+++ packs/basic/emojixx.js
@@ -1,5 +1,5 @@
 $.emojiarea.icons = {
-	':smile:'     : 'https://img.webme.com/pic/a/aliq03/smile.png',
+	';)'     : 'https://img.webme.com/pic/a/aliq03/smile.png',
 	':angry:'     : 'https://img.webme.com/pic/a/aliq03/angry.png',
 	':flushed:'   : 'https://img.webme.com/pic/a/aliq03/flushed.png',
 	':neckbeard:' : 'https://img.webme.com/pic/a/aliq03/neckbeard.png',
