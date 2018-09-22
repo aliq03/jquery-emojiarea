82e7e0abfbfc5a5c2c91a7d7b65694a3b6119bfd
aliq03 <aliq03@hotmail.com>
2018-09-22 10:03:13 +0300

    Update and rename emojixx.js to emojixs.js

---
+++ packs/basic/emojixs.js
@@ -0,0 +1,15 @@
+$.emojiarea.icons = {
+	' ;)'     : 'https://img.webme.com/pic/a/aliq03/1f609.png',
+	' :)'     : 'https://img.webme.com/pic/a/aliq03/1f603.png',
+	' :D'   : 'https://img.webme.com/pic/a/aliq03/1f605.png',
+	' >:(' : 'https://img.webme.com/pic/a/aliq03/1f479.png',
+	' :('  : 'https://img.webme.com/pic/a/aliq03/1f61e.png',
+	
+	' :o'     : 'https://img.webme.com/pic/a/aliq03/1f632.png',
+	' 8)'     : 'https://img.webme.com/pic/a/aliq03/1f913.png',
+	' ???'   : 'https://img.webme.com/pic/a/aliq03/2753.png',
+	' ;-)' : 'https://img.webme.com/pic/a/aliq03/1f644.png',
+	' :P' : 'https://img.webme.com/pic/a/aliq03/1f624.png',
+	
+	" :'("  : 'https://img.webme.com/pic/a/aliq03/1f622.png'
+};
--- packs/basic/emojixx.js
+++
@@ -1,7 +0,0 @@
-$.emojiarea.icons = {
-	';)'     : 'https://img.webme.com/pic/a/aliq03/smile.png',
-	':angry:'     : 'https://img.webme.com/pic/a/aliq03/angry.png',
-	':flushed:'   : 'https://img.webme.com/pic/a/aliq03/flushed.png',
-	':neckbeard:' : 'https://img.webme.com/pic/a/aliq03/neckbeard.png',
-	':laughing:'  : 'https://img.webme.com/pic/a/aliq03/laughing.png'
-};
